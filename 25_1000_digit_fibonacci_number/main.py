a87749d09e6b9760e549cd354ed31ac0d4cadf3a
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-01-16 22:51:52 +0100

    Project Euler 25 - 1000-digit Fibonacci number

---
+++ 25_1000_digit_fibonacci_number/main.py
@@ -0,0 +1,29 @@
+import copy
+import itertools
+
+
+def fibonacci_numbers_generator():
+    for _ in range(2):
+        yield "1"
+    previous_number = [1]
+    current_number = [1]
+    while True:
+        new_higher_number = []
+        add = 0
+        for upper_digit, lower_digit in itertools.zip_longest(current_number, previous_number, fillvalue=0):
+            add = upper_digit + lower_digit + add
+            new_higher_number.append(add % 10)
+            add = add//10
+        if add != 0:
+            new_higher_number.append(add)
+        previous_number = copy.copy(current_number)
+        current_number = copy.copy(new_higher_number)
+        yield "".join(reversed([str(digit) for digit in current_number]))
+
+
+if __name__ == "__main__":
+    number_of_digits = 1_000
+    for index, fibonacci_number in enumerate(fibonacci_numbers_generator(), 1):
+        if len(fibonacci_number) >= number_of_digits:
+            print(index)
+            break
