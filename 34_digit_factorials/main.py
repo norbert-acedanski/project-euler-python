e00e0f3627706b2db37eb11f0969d6173102b5c3
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-01-19 16:25:11 +0100

    Project Euler 34 - Digit factorials

---
+++ 34_digit_factorials/main.py
@@ -0,0 +1,25 @@
+def get_upper_limit_for_iteration() -> int:
+    """To get this value, we have to solve the inequality:
+       max_power*9! <= 10^max_power - 1"""
+    max_power = 1
+    while True:
+        if (limit := max_power*factorial(9)) <= 10**max_power - 1:
+            return limit
+        else:
+            max_power += 1
+
+
+def is_number_curious(number: int) -> bool:
+    digits = [int(digit) for digit in str(number)]
+    return number == sum(factorial(digit) for digit in digits)
+
+
+def factorial(number: int) -> int:
+    result = 1
+    for num in range(2, number + 1):
+        result *= num
+    return result
+
+
+if __name__ == "__main__":
+    print(sum(number for number in range(3, get_upper_limit_for_iteration() + 1) if is_number_curious(number=number)))
