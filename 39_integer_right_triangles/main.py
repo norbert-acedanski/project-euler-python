d7ed1b47ad37d4f91bc5e38926589d1e2cf92d76
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-01-22 15:48:09 +0100

    Add counting duplications of triples.

--- 39_integer_right_triangles/main.py
+++ 39_integer_right_triangles/main.py
@@ -2,10 +2,13 @@ def get_number_of_solutions_for_perimeter(perimeter_value: int) -> int:
     number_of_solutions = 0
     if perimeter_value % 2 != 0:
         return 0
+    duplicates = []
     for a in range(1, perimeter_value//2 + 1):
         for b in range(1, perimeter_value//2):
-            if is_triple_right_triangle(a, b, perimeter_value - a - b):
+            c = perimeter_value - a - b
+            if (a, b, c) not in duplicates and is_triple_right_triangle(a, b, c):
                 number_of_solutions += 1
+                duplicates.append((a, b, c))
     return number_of_solutions
 
 
