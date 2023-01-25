98cc2676dd61b63140172e4123b8f64745356c76
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-01-25 23:00:20 +0100

    Make first problem more elegant.

--- 1_multiples_of_3_or_5/main.py
+++ 1_multiples_of_3_or_5/main.py
@@ -1,9 +1,3 @@
-def multiple_of_3_or_5(up_to: int):
-    for number in range(1, up_to):
-        if number % 3 == 0 or number % 5 == 0:
-            yield number
-
-
 if __name__ == '__main__':
-    print(sum(multiple_of_3_or_5(1000)))
+    print(sum(number for number in range(1000) if number % 3 == 0 or number % 5 == 0))
 
