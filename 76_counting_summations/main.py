9098e0c1df682140cf2aa70e57b1f0f50bb59854
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-06-22 00:20:21 +0200

    Move method to common.py and update usage.

--- 76_counting_summations/main.py
+++ 76_counting_summations/main.py
@@ -1,27 +1,4 @@
-import functools
-import math
-
-
-def pentagonal_number(num: int) -> int:
-    return num*(3*num - 1)//2
-
-
-@functools.cache
-def number_of_partitions(number: int) -> int:
-    """Based on an equation p(n) = Sum (-1)**(k - 1)*p(n - gk), where k != 0 and gk is a pentagonal number.
-    https://en.wikipedia.org/wiki/Pentagonal_number_theorem"""
-    if number == 0:
-        return 1
-    num_of_partitions = 0
-    k_min = int((1 - math.sqrt(1 + 24*number))/6)
-    k_max = int((1 + math.sqrt(1 + 24*number))/6)
-    for k in range(k_min, k_max + 1):
-        if k == 0:
-            continue
-        coefficient = int((-1)**(k - 1))
-        num_of_partitions += coefficient*number_of_partitions(number - pentagonal_number(k))
-    return num_of_partitions
-
+from common import number_of_partitions
 
 if __name__ == "__main__":
     print(number_of_partitions(100) - 1)
--- common.py
+++ common.py
@@ -1,3 +1,4 @@
+import functools
 import itertools
 import math
 import string
@@ -111,3 +112,24 @@ def get_reduced_fractions(d: int) -> List[Tuple[int, int]]:
             g = math.gcd(numerator, denominator)
             reduced_fractions.add((numerator//g, denominator//g))
     return sorted(reduced_fractions, key=lambda fraction: fraction[0]/fraction[1])
+
+
+def pentagonal_number(num: int) -> int:
+    return num*(3*num - 1)//2
+
+
+@functools.cache
+def number_of_partitions(number: int) -> int:
+    """Based on an equation p(n) = Sum (-1)**(k - 1)*p(n - gk), where k != 0 and gk is a pentagonal number.
+    https://en.wikipedia.org/wiki/Pentagonal_number_theorem"""
+    if number == 0:
+        return 1
+    num_of_partitions = 0
+    k_min = int((1 - math.sqrt(1 + 24*number))/6)
+    k_max = int((1 + math.sqrt(1 + 24*number))/6)
+    for k in range(k_min, k_max + 1):
+        if k == 0:
+            continue
+        coefficient = int((-1)**(k - 1))
+        num_of_partitions += coefficient*number_of_partitions(number - pentagonal_number(k))
+    return num_of_partitions
